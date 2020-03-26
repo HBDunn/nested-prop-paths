b0e1ab4d272613f84379bec8d081aca0e229c869
hdunn <hdunn.id@gmail.com>
2020-03-26 16:50:42 -0600

    fix npm packaging

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.propPaths = require("./src/nested-prop-paths").propPaths;
+exports.getVals = require("./src/nested-prop-paths").getVals;
+exports.find = require("./src/nested-prop-paths").find;
+exports.runExample = require("./src/example/try").runExample;
